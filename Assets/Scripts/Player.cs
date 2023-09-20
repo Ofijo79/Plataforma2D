669da9215bf216a4cc2aac3d716655ef550f9002
Ofijo79 <oriolfijo2004@gmail.com>
2023-09-20 11:03:12 +0200

    Player and ground sensor

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,12 +6,20 @@ public class Player : MonoBehaviour
 {
     [SerializeField]float _playerSpeed = 5;
     float _playerInputX;
-    float _playerInputY;
+    //float _playerInputY;
+    [SerializeField]float _jumpForce = 5;
+    
+
+    Rigidbody2D _rBody2D;
+
+    GroundSensor _sensor;
 
     
     // Start is called before the first frame update
     void Start()
     {
+        _rBody2D = GetComponent<Rigidbody2D>();
+        _sensor = GetComponentInChildren<GroundSensor>();
         
     }
 
@@ -19,14 +27,31 @@ public class Player : MonoBehaviour
     void Update()
     {
         PlayerMovement();
+
+        if(Input.GetButtonDown("Jump") && _sensor._isGrounded)
+        {
+            Jump();
+        }
+    }
+
+    void FixedUpdate() 
+    {
+        //_rBody2D.AddForce(new Vector2(_playerInputX * _playerSpeed, 0), ForceMode2D.Impulse);
+
+        _rBody2D.velocity = new Vector2(_playerInputX * _playerSpeed, _rBody2D.velocity.y);
     }
 
     void PlayerMovement()
     {
         _playerInputX = Input.GetAxis("Horizontal");
-        _playerInputY = Input.GetAxis("Vertical");
+        /*_playerInputY = Input.GetAxis("Vertical");
 
-        transform.Translate(new Vector2(_playerInputX, _playerInputY) * _playerSpeed  * Time.deltaTime);
+        transform.Translate(new Vector2(_playerInputX, _playerInputY) * _playerSpeed  * Time.deltaTime);*/
     }
     
+    void Jump()
+    {
+        _rBody2D.AddForce(Vector2.up *_jumpForce, ForceMode2D.Impulse);
+    }
 }
+
